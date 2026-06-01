7e64308a759cd9410e6dc46b239c1b5386532000
TufailMd <jrtufailmd@gmail.com>
2026-06-01 11:46:36 +0530

    Add logo images and update settings for code formatting
    - Added a PNG logo image to the public/images directory.
    - Added an SVG logo image to the public/images directory.
    - Created a settings.json file to configure editor settings, including Prettier for code formatting and ESLint integration.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div>This is normal font </div>
+      <div className="font-inter">This is Inter font And this</div>
+      <div className="font-inter-i">This is Inter Italic font</div>
+      <div className="font-space-grotesk">This is Space Grotesk font</div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 }
 
 export default page;
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import Theme from "./Theme";
+import Logo from "../../../public/images/logo-site.svg";
+
+function Navbar() {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed x-50 w-full  gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src={Logo} width={35} height={35} alt="StudyFlow Logo" />
+
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Study<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+
+      <p>Search</p>
+
+      {/* <div className="h-6 w-6 rounded-full bg-primary-500 text-center text-xs text-light-900 flex"> */}
+      <button className="flex-between gap-5 cursor-pointer  rounded-full bg-primary-500 p-2 text-light-900">
+        Ask Questions
+      </button>
+
+      <div className="flex-between gap-5 cursor-pointer  rounded-full bg-primary-500 p-2 text-light-900">
+        Notification
+      </div>
+
+      <div className="flex-between gap-5 cursor-pointer">
+        <Theme />
+      </div>
+      <div className="flex-between gap-5 cursor-pointer  rounded-full bg-primary-500 p-2 text-light-900">
+        profile
+      </div>
+      {/* </div> */}
+    </nav>
+  );
+}
+
+export default Navbar;
